5bda5fc00108af4ceefa26f5f7f9750b5c66697e
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-07 14:14:00 -0300

    test: Unit test to get /bookings

--- tests/unit/booking-service.test.ts
+++ tests/unit/booking-service.test.ts
@@ -7,12 +7,25 @@ describe('BookingService unit test suite', () => {
   describe('getUserBookings service', () => {
     it('Should return a user booking', async () => {
       const userId = faker.datatype.number({ min: 1, max: 30 });
-      const booking = createMockBooking();
+      const booking = await createMockBooking();
 
       jest.spyOn(bookingsRepository, 'getUserBookings').mockResolvedValueOnce(booking);
 
-      const response = bookingsService.getUserBookings(userId);
+      const response = await bookingsService.getUserBookings(userId);
       expect(response).toEqual(booking);
+    });
+
+    it('When booking does not exist: throw a NotFoundError', async () => {
+      const userId = faker.datatype.number({ min: 1, max: 30 });
+
+      jest.spyOn(bookingsRepository, 'getUserBookings').mockResolvedValueOnce(undefined);
+
+      const response = bookingsService.getUserBookings(userId);
+      console.log(response);
+      expect(response).rejects.toEqual({
+        name: 'NotFoundError',
+        message: 'No result for this search!',
+      });
     });
   });
 });
